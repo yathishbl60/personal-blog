d2dc1d63d8c2ce8e02eb51562b3d9abf1dfe4c63
Yatheesha B L <yathishbl60@gmail.com>
2016-05-01 18:48:42 +0530

    Edit and delete project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -19,7 +19,22 @@ class ProjectsController < ApplicationController
 	end
 
 	def show
-		
+	end
+
+	def edit
+	end
+
+	def update
+		if @project.update project_params
+			redirect_to @project, notice: "Project is successfully updated"
+		else 
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@project.destroy
+		redirect_to projects_path
 	end
 
 	private
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,5 @@ class WelcomeController < ApplicationController
 	def index
 		@posts = Post.all.limit(3).order("created_at desc")
 		@projects = Project.all.limit(3).order("created_at desc")
-
 	end
 end
